921892841b5bbb34b9cb13a7e466a08419936e72
Jason Wang <zhengang@amazon.com>
2019-12-05 14:10:58 -0800

    connect kernel by http server

---
+++ src/main/java/com/aws/iot/evergreen/cli/adapter/KernelAdapter.java
@@ -0,0 +1,21 @@
+package com.aws.iot.evergreen.cli.adapter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public interface KernelAdapter {
+
+    Map<String, String> getConfigs(Set<String> configPaths);
+
+    void setConfigs(Map<String, String> configs);
+
+    String healthPing();
+
+    Map<String, Map<String, String>> getServicesStatus(Set<String> serviceNames);
+
+    Map<String, Map<String, String>> reloadServices(Set<String> serviceNames);
+
+    Map<String, Map<String, String>> closeServices(Set<String> serviceNames);
+
+}

90d7a263719df0309a2bfe259efabf2c77913801
Mohammed <faham@amazon.com>
2020-09-10 15:50:00 -0700

    cosmetic changes/ minor refactor

--- src/main/java/com/aws/iot/evergreen/cli/adapter/impl/KernelAdapterIpcClientImpl.java
+++ src/main/java/com/aws/iot/evergreen/cli/adapter/impl/KernelAdapterIpcClientImpl.java
@@ -23,7 +23,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
-
 import javax.annotation.Nullable;
 import java.io.IOException;
 import java.net.URI;
@@ -44,12 +43,71 @@ public class KernelAdapterIpcClientImpl implements KernelAdapterIpc {
     private static final String CLI_AUTH_TOKEN = "cli_auth_token";
     private static final String SOCKET_URL = "socket_url";
 
-    private IPCClient ipcClient;
+    private String root;
     private Cli cliClient;
 
     @Inject
-    public KernelAdapterIpcClientImpl(@Nullable @Named("ggcRootPath") String root)
-            throws InterruptedException, IPCClientException, IOException, URISyntaxException {
+    public KernelAdapterIpcClientImpl(@Nullable @Named("ggcRootPath") String root) {
+        this.root = root;
+    }
+
+    @Override
+    public ComponentDetails getComponentDetails(String componentName)
+            throws CliIpcClientException, GenericCliIpcServerException {
+        return getCliClient().getComponentDetails(GetComponentDetailsRequest.builder()
+                .componentName(componentName).build()).getComponentDetails();
+    }
+
+    @Override
+    public void restartComponent(String componentName) throws GenericCliIpcServerException, CliIpcClientException {
+        getCliClient().restartComponent(RestartComponentRequest.builder().componentName(componentName).build());
+    }
+
+    @Override
+    public void stopComponent(String componentName) throws GenericCliIpcServerException, CliIpcClientException {
+        getCliClient().stopComponent(StopComponentRequest.builder().componentName(componentName).build());
+    }
+
+    @Override
+    public void updateRecipesAndArtifacts(String recipesDirectoryPath, String artifactsDirectoryPath)
+            throws GenericCliIpcServerException, CliIpcClientException {
+        UpdateRecipesAndArtifactsRequest updateRecipesAndArtifactsRequest = UpdateRecipesAndArtifactsRequest.builder()
+                .recipeDirectoryPath(recipesDirectoryPath)
+                .artifactDirectoryPath(artifactsDirectoryPath)
+                .build();
+        getCliClient().updateRecipesAndArtifacts(updateRecipesAndArtifactsRequest);
+    }
+
+    @Override
+    public String createLocalDeployment(CreateLocalDeploymentRequest createLocalDeploymentRequest)
+            throws GenericCliIpcServerException, CliIpcClientException {
+        return getCliClient().createLocalDeployment(createLocalDeploymentRequest).getDeploymentId();
+    }
+
+    @Override
+    public List<ComponentDetails> listComponents() throws GenericCliIpcServerException, CliIpcClientException {
+        ListComponentsResponse listComponentsResponse = getCliClient().listComponents();
+        return listComponentsResponse.getComponents();
+    }
+
+    @Override
+    public LocalDeployment getLocalDeploymentStatus(String deploymentId)
+            throws GenericCliIpcServerException, CliIpcClientException {
+        GetLocalDeploymentStatusRequest DeploymentStatusRequest =
+                GetLocalDeploymentStatusRequest.builder().deploymentId(deploymentId).build();
+        return getCliClient().getLocalDeploymentStatus(DeploymentStatusRequest).getDeployment();
+    }
+
+    @Override
+    public List<LocalDeployment> listLocalDeployments()
+            throws GenericCliIpcServerException, CliIpcClientException {
+        return getCliClient().listLocalDeployments().getLocalDeployments();
+    }
+
+    private Cli getCliClient() {
+        if (cliClient != null) {
+            return cliClient;
+        }
         // TODO: When the greengrass-cli is installed in the kernel root path this will derived from the current working
         // directory, instead of an env variable. Until then using env variable.
         // check if root path was passed as an argument to the command line, else fall back to env variable
@@ -63,68 +121,22 @@ public class KernelAdapterIpcClientImpl implements KernelAdapterIpc {
         if (!Files.exists(filepath)) {
             throw new RuntimeException("CLI IPC info file not present");
         }
-        Map<String, String> ipcInfoMap = OBJECT_MAPPER.readValue(Files.readAllBytes(filepath), HashMap.class);
-        String socketUrl = ipcInfoMap.get(SOCKET_URL);
-        String token = ipcInfoMap.get(CLI_AUTH_TOKEN);
-        URI uri = new URI(socketUrl);
-        ipcClient = new IPCClientImpl(KernelIPCClientConfig.builder()
-                .hostAddress(uri.getHost())
-                .port(uri.getPort())
-                .token(token)
-                .build());
-        cliClient = new CliImpl(ipcClient);
-    }
-
-    @Override
-    public ComponentDetails getComponentDetails(String componentName)
-            throws CliIpcClientException, GenericCliIpcServerException {
-        return cliClient.getComponentDetails(GetComponentDetailsRequest.builder()
-                .componentName(componentName).build()).getComponentDetails();
-    }
-
-    @Override
-    public void restartComponent(String componentName) throws GenericCliIpcServerException, CliIpcClientException {
-        cliClient.restartComponent(RestartComponentRequest.builder().componentName(componentName).build());
-    }
-
-    @Override
-    public void stopComponent(String componentName) throws GenericCliIpcServerException, CliIpcClientException {
-        cliClient.stopComponent(StopComponentRequest.builder().componentName(componentName).build());
-    }
-
-    @Override
-    public void updateRecipesAndArtifacts(String recipesDirectoryPath, String artifactsDirectoryPath)
-            throws GenericCliIpcServerException, CliIpcClientException {
-        UpdateRecipesAndArtifactsRequest updateRecipesAndArtifactsRequest = UpdateRecipesAndArtifactsRequest.builder()
-                .recipeDirectoryPath(recipesDirectoryPath)
-                .artifactDirectoryPath(artifactsDirectoryPath)
-                .build();
-        cliClient.updateRecipesAndArtifacts(updateRecipesAndArtifactsRequest);
-    }
-
-    @Override
-    public String createLocalDeployment(CreateLocalDeploymentRequest createLocalDeploymentRequest)
-            throws GenericCliIpcServerException, CliIpcClientException {
-        return cliClient.createLocalDeployment(createLocalDeploymentRequest).getDeploymentId();
-    }
-
-    @Override
-    public List<ComponentDetails> listComponents() throws GenericCliIpcServerException, CliIpcClientException {
-        ListComponentsResponse listComponentsResponse = cliClient.listComponents();
-        return listComponentsResponse.getComponents();
-    }
-
-    @Override
-    public LocalDeployment getLocalDeploymentStatus(String deploymentId)
-            throws GenericCliIpcServerException, CliIpcClientException {
-        GetLocalDeploymentStatusRequest DeploymentStatusRequest =
-                GetLocalDeploymentStatusRequest.builder().deploymentId(deploymentId).build();
-        return cliClient.getLocalDeploymentStatus(DeploymentStatusRequest).getDeployment();
-    }
-
-    @Override
-    public List<LocalDeployment> listLocalDeployments()
-            throws GenericCliIpcServerException, CliIpcClientException {
-        return cliClient.listLocalDeployments().getLocalDeployments();
+        try {
+            Map<String, String> ipcInfoMap = OBJECT_MAPPER.readValue(Files.readAllBytes(filepath), HashMap.class);
+            String socketUrl = ipcInfoMap.get(SOCKET_URL);
+            String token = ipcInfoMap.get(CLI_AUTH_TOKEN);
+            URI uri = new URI(socketUrl);
+            IPCClient ipcClient = new IPCClientImpl(KernelIPCClientConfig.builder()
+                    .hostAddress(uri.getHost())
+                    .port(uri.getPort())
+                    .token(token)
+                    .build());
+            cliClient = new CliImpl(ipcClient);
+        } catch (Exception e) {
+            System.out.println("Unable to create cli client " + e.getMessage());
+            e.printStackTrace(System.err);
+            System.exit(1);
+        }
+        return cliClient;
     }
 }

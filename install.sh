83447a80d5ed0d833e57d647b0430f9b9abb5ab9
Michael Dombrowski <mdombro@amazon.com>
2020-10-30 20:38:41 -0700

    Remove CLI completion script (#57)

--- install.sh
+++ install.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env sh
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0" &2>/dev/null || echo "$(pwd)/""$0")")"
-if [ "$(uname)" != "Darwin" ]; then
-    echo "Setting up auto-complete for Greengrass CLI."
-    # Autocomplete is a feature provided by picocli, which is currently NOT supported on Mac https://github.com/remkop/picocli/issues/396
-
-    if [ -n "$ZSH_VERSION" ]; then
-        echo "Allow zsh to read bash completion specifications and functions"
-        autoload -U +X compinit && compinit
-        autoload -U +X bashcompinit && bashcompinit
-    fi
-
-    if [ -n "$BASH_VERSION" ]; then
-        echo "Source the bash completion script"
-        bash -c 'source $SCRIPT_DIR/cli_completion.sh'
-    fi
-fi
 ln -fs $SCRIPT_DIR/bin/greengrass-cli /usr/local/bin/greengrass-cli &2>/dev/null || sudo ln -fs $SCRIPT_DIR/bin/greengrass-cli /usr/local/bin/greengrass-cli
 echo "Start using greengrass-cli"

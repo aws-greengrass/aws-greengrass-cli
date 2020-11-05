90ba7f1bf5b3687f49599cd62c6bba79ce68b80a
fahadmohammed01 <52720275+fahadmohammed01@users.noreply.github.com>
2020-11-04 16:45:02 -0800

    Moved CLI to new IPC SDK (#54)
    * Initial commit to move to new ipc client
    
    * commit adapter
    
    * checked in ipc sdk jar
    
    * checked in crt jar
    
    * updated sdk jar and added time out
    
    * added timeout for getting connection
    
    * Using symlink when connecting to IPC socket to prevent invalid socket error
    
    * Increasing default timeout of CLI connection setup
    
    * added back --params as a dummy input
    
    Co-authored-by: Banthiya <abanthiy@amazon.com>

--- src/main/java/com/aws/greengrass/cli/commands/ComponentCommand.java
+++ src/main/java/com/aws/greengrass/cli/commands/ComponentCommand.java
@@ -6,17 +6,12 @@
 package com.aws.greengrass.cli.commands;
 
 import com.aws.greengrass.cli.adapter.NucleusAdapterIpc;
-import com.aws.greengrass.ipc.services.cli.exceptions.CliIpcClientException;
-import com.aws.greengrass.ipc.services.cli.exceptions.GenericCliIpcServerException;
-import com.aws.greengrass.ipc.services.cli.models.ComponentDetails;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
+import software.amazon.awssdk.aws.greengrass.model.ComponentDetails;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.inject.Inject;
 
 @CommandLine.Command(name = "component", resourceBundle = "com.aws.greengrass.cli.CLI_messages",
@@ -27,15 +22,13 @@ public class ComponentCommand extends BaseCommand {
     private final NucleusAdapterIpc nucleusAdapterIpc;
 
     @Inject
-    public ComponentCommand(
-            NucleusAdapterIpc nucleusAdapterIpc
-    ) {
+    public ComponentCommand(NucleusAdapterIpc nucleusAdapterIpc) {
         this.nucleusAdapterIpc = nucleusAdapterIpc;
     }
 
+
     @CommandLine.Command(name = "restart", mixinStandardHelpOptions = true)
-    public int restart(@CommandLine.Option(names = {"-n", "--names"}, paramLabel = "component names separated by comma", descriptionKey = "names", required = true) String names)
-            throws CliIpcClientException, GenericCliIpcServerException {
+    public int restart(@CommandLine.Option(names = {"-n", "--names"}, paramLabel = "component names separated by comma", descriptionKey = "names", required = true) String names) {
         String[] componentNames = names.split(" *[&,]+ *");
         for (String componentName : componentNames) {
             nucleusAdapterIpc.restartComponent(componentName);
@@ -43,9 +36,9 @@ public class ComponentCommand extends BaseCommand {
         return 0;
     }
 
+
     @CommandLine.Command(name = "stop", mixinStandardHelpOptions = true)
-    public int stop(@CommandLine.Option(names = {"-n", "--names"}, paramLabel = "component names separated by comma", descriptionKey = "names", required = true) String names)
-            throws CliIpcClientException, GenericCliIpcServerException {
+    public int stop(@CommandLine.Option(names = {"-n", "--names"}, paramLabel = "component names separated by comma", descriptionKey = "names", required = true) String names) {
         String[] componentNames = names.split(" *[&,]+ *");
         for (String componentName : componentNames) {
             nucleusAdapterIpc.stopComponent(componentName);
@@ -53,10 +46,11 @@ public class ComponentCommand extends BaseCommand {
         return 0;
     }
 
+
     // GG_NEEDS_REVIEW: TODO: input validation and better error handling https://sim.amazon.com/issues/P39478724
     @CommandLine.Command(name = "list", mixinStandardHelpOptions = true,
             description = "Prints root level components names, component information and runtime parameters")
-    public int list() throws CliIpcClientException, GenericCliIpcServerException, JsonProcessingException {
+    public int list() throws JsonProcessingException {
         List<ComponentDetails> componentDetails = nucleusAdapterIpc.listComponents();
         System.out.println("Components currently running in Greengrass:");
         for (ComponentDetails c : componentDetails) {
@@ -68,7 +62,7 @@ public class ComponentCommand extends BaseCommand {
     // GG_NEEDS_REVIEW: TODO: input validation and better error handling https://sim.amazon.com/issues/P39478724
     @CommandLine.Command(name = "details", mixinStandardHelpOptions = true)
     public int details(@CommandLine.Option(names = {"-n", "--name"}, paramLabel = " component name", descriptionKey = "name", required = true) String componentName)
-            throws CliIpcClientException, GenericCliIpcServerException, JsonProcessingException {
+            throws JsonProcessingException {
         ComponentDetails componentDetails = nucleusAdapterIpc.getComponentDetails(componentName);
         printComponentDetails(componentDetails);
         return 0;
@@ -79,45 +73,5 @@ public class ComponentCommand extends BaseCommand {
         System.out.println("Version: " + component.getVersion());
         System.out.println("State: " + component.getState());
         System.out.println("Configuration: " + mapper.writeValueAsString(component.getConfiguration()));
-        System.out.println("Configurations: " + mapper.writeValueAsString(component.getNestedConfiguration()));
-    }
-
-    /**
-     * Convert parameters. For example: {Component:path.key: value} -> {Component: {path: {key: {value}}}}
-     *
-     * @param params runtime parameters in the flat map
-     * @return converted runtime parameters map, with each key as component name and each value as the component's
-     *         configuration map
-     */
-    @Deprecated
-     static Map<String, Map<String, Object>> convertParameters(Map<String, String> params) {
-        if (params == null || params.isEmpty()) {
-            return Collections.emptyMap();
-        }
-        Map<String, Map<String, Object>> componentNameToConfig = new HashMap<>();
-
-        for (Map.Entry<String, String> entry : params.entrySet()) {
-            String componentNameAndKey = entry.getKey();
-            String value = entry.getValue();
-            String[] parts = componentNameAndKey.split(":");
-
-            if (parts.length != 2) {
-                throw new IllegalArgumentException("--param is not in the format of <ComponentName>:<key>=<value>");
-            }
-            String componentName = parts[0];
-            String multiLevelKey = parts[1];
-            String[] levels = multiLevelKey.split("\\.");
-
-            componentNameToConfig.putIfAbsent(componentName, new HashMap<>());
-            HashMap map = (HashMap) componentNameToConfig.get(componentName);
-            String key;
-            for (int i = 0; i < levels.length - 1; i++) {
-                key = levels[i];
-                map.putIfAbsent(key, new HashMap<>());
-                map = (HashMap<Object, Object>) map.get(key);
-            }
-            map.put(levels[levels.length - 1], value);
-        }
-        return componentNameToConfig;
     }
 }
--- src/main/java/com/aws/greengrass/cli/commands/DeploymentCommand.java
+++ src/main/java/com/aws/greengrass/cli/commands/DeploymentCommand.java
@@ -6,13 +6,11 @@
 package com.aws.greengrass.cli.commands;
 
 import com.aws.greengrass.cli.adapter.NucleusAdapterIpc;
-import com.aws.greengrass.ipc.services.cli.exceptions.CliIpcClientException;
-import com.aws.greengrass.ipc.services.cli.exceptions.GenericCliIpcServerException;
-import com.aws.greengrass.ipc.services.cli.models.CreateLocalDeploymentRequest;
-import com.aws.greengrass.ipc.services.cli.models.LocalDeployment;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
+import software.amazon.awssdk.aws.greengrass.model.CreateLocalDeploymentRequest;
+import software.amazon.awssdk.aws.greengrass.model.LocalDeployment;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -24,19 +22,16 @@ import java.util.Map;
 import javax.inject.Inject;
 
 import static com.aws.greengrass.cli.adapter.impl.NucleusAdapterIpcClientImpl.deTilde;
-import static com.aws.greengrass.cli.commands.ComponentCommand.convertParameters;
 
 @CommandLine.Command(name = "deployment", resourceBundle = "com.aws.greengrass.cli.CLI_messages",
         subcommands = CommandLine.HelpCommand.class, mixinStandardHelpOptions = true)
 public class DeploymentCommand extends BaseCommand {
 
     private final ObjectMapper mapper = new ObjectMapper();
-    private final NucleusAdapterIpc nucleusAdapterIpc;
+    private NucleusAdapterIpc nucleusAdapterIpc;
 
     @Inject
-    public DeploymentCommand(
-            NucleusAdapterIpc nucleusAdapterIpc
-    ) {
+    public DeploymentCommand(NucleusAdapterIpc nucleusAdapterIpc) {
         this.nucleusAdapterIpc = nucleusAdapterIpc;
     }
 
@@ -52,9 +47,8 @@ public class DeploymentCommand extends BaseCommand {
      @CommandLine.Option(names = {"-a", "--artifactDir"}, paramLabel = "Artifacts Folder Path") String artifactDir,
      @CommandLine.Option(names = {"-p", "--param"}, paramLabel = "Runtime parameters") Map<String, String> parameters,
      @CommandLine.Option(names = {"-c", "--update-config"}, paramLabel = "Update configuration") String configUpdate)
-            throws CliIpcClientException, GenericCliIpcServerException, IOException {
+            throws IOException {
         // GG_NEEDS_REVIEW: TODO Validate folder exists and folder structure
-        Map<String, Map<String, Object>> componentNameToConfig = convertParameters(parameters);
         Map<String, Map<String, Object>> configurationUpdate = null;
         if (configUpdate != null && !configUpdate.isEmpty()) {
             try {
@@ -81,13 +75,13 @@ public class DeploymentCommand extends BaseCommand {
         if (recipeDir != null || artifactDir != null) {
             nucleusAdapterIpc.updateRecipesAndArtifacts(recipeDir, artifactDir);
         }
-        CreateLocalDeploymentRequest createLocalDeploymentRequest = CreateLocalDeploymentRequest.builder()
-                .groupName(groupId)
-                .configurationUpdate(configurationUpdate)
-                .componentToConfiguration(componentNameToConfig)
-                .rootComponentVersionsToAdd(componentsToMerge)
-                .rootComponentsToRemove(componentsToRemove)
-                .build();
+
+        CreateLocalDeploymentRequest createLocalDeploymentRequest = new CreateLocalDeploymentRequest();
+        createLocalDeploymentRequest.setGroupName(groupId);
+        createLocalDeploymentRequest.setComponentToConfiguration(configurationUpdate);
+        createLocalDeploymentRequest.setRootComponentVersionsToAdd(componentsToMerge);
+        createLocalDeploymentRequest.setRootComponentsToRemove(componentsToRemove);
+
         String deploymentId = nucleusAdapterIpc.createLocalDeployment(createLocalDeploymentRequest);
         System.out.println("Local deployment has been submitted! Deployment Id: " + deploymentId);
         return 0;
@@ -96,8 +90,7 @@ public class DeploymentCommand extends BaseCommand {
     // GG_NEEDS_REVIEW: TODO: input validation and better error handling https://sim.amazon.com/issues/P39478724
     @CommandLine.Command(name = "status",
             description = "Retrieve the status of a deployment", mixinStandardHelpOptions = true)
-    public int status(@CommandLine.Option(names = {"-i", "--deploymentId"}, paramLabel = "Deployment Id", required = true) String deploymentId)
-            throws CliIpcClientException, GenericCliIpcServerException {
+    public int status(@CommandLine.Option(names = {"-i", "--deploymentId"}, paramLabel = "Deployment Id", required = true) String deploymentId) {
 
         LocalDeployment status = nucleusAdapterIpc.getLocalDeploymentStatus(deploymentId);
         System.out.printf("%s: %s", status.getDeploymentId(), status.getStatus());
@@ -107,7 +100,7 @@ public class DeploymentCommand extends BaseCommand {
     // GG_NEEDS_REVIEW: TODO: input validation and better error handling https://sim.amazon.com/issues/P39478724
     @CommandLine.Command(name = "list", description = "Retrieve the status of local deployments",
             mixinStandardHelpOptions = true)
-    public int list() throws CliIpcClientException, GenericCliIpcServerException {
+    public int list() {
         List<LocalDeployment> localDeployments = nucleusAdapterIpc.listLocalDeployments();
         localDeployments.forEach((status) -> System.out.println(status.getDeploymentId() + ": " + status.getStatus()));
         return 0;

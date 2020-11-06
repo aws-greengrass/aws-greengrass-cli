699c55b31cca6e1b42a482a6112ffb76d2b8efe9
Hui Yang <huiyn@amazon.com>
2020-11-05 18:44:29 -0800

    Address comments; clean up

--- server/src/main/java/com/aws/greengrass/cli/CLIService.java
+++ server/src/main/java/com/aws/greengrass/cli/CLIService.java
@@ -12,29 +12,8 @@ import com.aws.greengrass.dependency.State;
 import com.aws.greengrass.deployment.DeploymentStatusKeeper;
 import com.aws.greengrass.deployment.model.Deployment;
 import com.aws.greengrass.ipc.AuthenticationHandler;
-import com.aws.greengrass.ipc.ConnectionContext;
-import com.aws.greengrass.ipc.IPCRouter;
-import com.aws.greengrass.ipc.common.BuiltInServiceDestinationCode;
-import com.aws.greengrass.ipc.common.FrameReader;
-import com.aws.greengrass.ipc.exceptions.IPCException;
 import com.aws.greengrass.ipc.exceptions.UnauthenticatedException;
-import com.aws.greengrass.ipc.services.cli.CliClientOpCodes;
-import com.aws.greengrass.ipc.services.cli.exceptions.ComponentNotFoundError;
-import com.aws.greengrass.ipc.services.cli.exceptions.GenericCliIpcServerException;
-import com.aws.greengrass.ipc.services.cli.exceptions.InvalidArgumentsError;
-import com.aws.greengrass.ipc.services.cli.exceptions.InvalidArtifactsDirectoryPathError;
-import com.aws.greengrass.ipc.services.cli.exceptions.InvalidRecipesDirectoryPathError;
-import com.aws.greengrass.ipc.services.cli.exceptions.ResourceNotFoundError;
-import com.aws.greengrass.ipc.services.cli.exceptions.ServiceError;
-import com.aws.greengrass.ipc.services.cli.models.CliGenericResponse;
-import com.aws.greengrass.ipc.services.cli.models.CreateLocalDeploymentRequest;
 import com.aws.greengrass.ipc.services.cli.models.DeploymentStatus;
-import com.aws.greengrass.ipc.services.cli.models.GetComponentDetailsRequest;
-import com.aws.greengrass.ipc.services.cli.models.GetLocalDeploymentStatusRequest;
-import com.aws.greengrass.ipc.services.cli.models.RestartComponentRequest;
-import com.aws.greengrass.ipc.services.cli.models.StopComponentRequest;
-import com.aws.greengrass.ipc.services.cli.models.UpdateRecipesAndArtifactsRequest;
-import com.aws.greengrass.ipc.services.common.ApplicationMessage;
 import com.aws.greengrass.lifecyclemanager.GreengrassService;
 import com.aws.greengrass.lifecyclemanager.Kernel;
 import com.aws.greengrass.util.Coerce;
@@ -42,7 +21,6 @@ import com.aws.greengrass.util.Exec;
 import com.aws.greengrass.util.FileSystemPermission;
 import com.aws.greengrass.util.platforms.Platform;
 import com.aws.greengrass.util.platforms.UserPlatform;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.cbor.databind.CBORMapper;
@@ -58,8 +36,6 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
 import javax.inject.Inject;
 
 import static com.aws.greengrass.componentmanager.KernelConfigResolver.PARAMETERS_CONFIG_KEY;
@@ -90,12 +66,6 @@ public class CLIService extends GreengrassService {
     private final Map<String, String> clientIdToAuthToken = new HashMap<>();
 
     @Inject
-    CLIServiceAgent agent;
-
-    @Inject
-    private IPCRouter router;
-
-    @Inject
     private DeploymentStatusKeeper deploymentStatusKeeper;
 
     @Inject
@@ -118,21 +88,16 @@ public class CLIService extends GreengrassService {
      * Constructor for unit testing.
      * @param topics Service config
      * @param privateConfig Private config for the service
-     * @param router {@link IPCRouter}
-     * @param agent {@link CLIServiceAgent}
      * @param cliEventStreamAgent {@link CLIEventStreamAgent}
      * @param deploymentStatusKeeper {@link DeploymentStatusKeeper}
      * @param authenticationHandler {@link AuthenticationHandler}
      * @param kernel {@link Kernel}
      * @param greengrassCoreIPCService {@link GreengrassCoreIPCService}
      */
-    public CLIService(Topics topics, Topics privateConfig, IPCRouter router, CLIServiceAgent agent,
-                      CLIEventStreamAgent cliEventStreamAgent,
+    public CLIService(Topics topics, Topics privateConfig, CLIEventStreamAgent cliEventStreamAgent,
                       DeploymentStatusKeeper deploymentStatusKeeper, AuthenticationHandler authenticationHandler,
                       Kernel kernel, GreengrassCoreIPCService greengrassCoreIPCService) {
         super(topics, privateConfig);
-        this.router = router;
-        this.agent = agent;
         this.cliEventStreamAgent = cliEventStreamAgent;
         this.deploymentStatusKeeper = deploymentStatusKeeper;
         this.authenticationHandler = authenticationHandler;
@@ -142,23 +107,11 @@ public class CLIService extends GreengrassService {
 
     @Override
     public void postInject() {
-        BuiltInServiceDestinationCode destination = BuiltInServiceDestinationCode.CLI;
         super.postInject();
         // Does not happen for built-in/plugin services so doing explicitly
         AuthenticationHandler.registerAuthenticationToken(this);
-        try {
-            router.registerServiceCallback(destination.getValue(), this::handleMessage);
-            logger.atInfo().setEventType("ipc-register-request-handler").addKeyValue("destination", destination.name())
-                    .log();
-            deploymentStatusKeeper.registerDeploymentStatusConsumer(Deployment.DeploymentType.LOCAL,
+        deploymentStatusKeeper.registerDeploymentStatusConsumer(Deployment.DeploymentType.LOCAL,
                     this::deploymentStatusChanged, CLIService.class.getName());
-
-        } catch (IPCException e) {
-            logger.atError().setEventType("ipc-register-request-handler-error").setCause(e)
-                    .addKeyValue("destination", destination.name())
-                    .log("Failed to register service callback to destination");
-        }
-
         config.lookup(PARAMETERS_CONFIG_KEY, posixGroups).subscribe((why, newv) -> {
             requestRestart();
         });
@@ -343,143 +296,5 @@ public class CLIService extends GreengrassService {
     protected Boolean deploymentStatusChanged(Map<String, Object> deploymentDetails) {
         cliEventStreamAgent.persistLocalDeployment(config, deploymentDetails);
         return true;
-    }
-
-
-    /**
-     * Handle all requests for CLI from the CLI client.
-     *
-     * @param message incoming request
-     * @param context Context identifying the client and the channel
-     */
-    @SuppressWarnings("PMD.AvoidCatchingThrowable")
-    public Future<FrameReader.Message> handleMessage(FrameReader.Message message, ConnectionContext context) {
-        CompletableFuture<FrameReader.Message> fut = new CompletableFuture<>();
-
-        ApplicationMessage applicationMessage = ApplicationMessage.fromBytes(message.getPayload());
-        try {
-            // GG_NEEDS_REVIEW: TODO: add version compatibility check
-            CliClientOpCodes opCode = CliClientOpCodes.values()[applicationMessage.getOpCode()];
-            ApplicationMessage responseMessage = null;
-            switch (opCode) {
-                case GET_COMPONENT_DETAILS: {
-
-                    GetComponentDetailsRequest request = CBOR_MAPPER
-                            .readValue(applicationMessage.getPayload(), GetComponentDetailsRequest.class);
-                    try {
-                        CliGenericResponse cliGenericResponse = agent.getComponentDetails(request);
-                        responseMessage = getSuccessfulResponseMessage(cliGenericResponse, applicationMessage
-                                .getVersion());
-                    } catch (InvalidArgumentsError | ComponentNotFoundError e) {
-                        responseMessage = getErrorResponseMessage(e, applicationMessage.getVersion());
-                    }
-                    break;
-                }
-                case LIST_COMPONENTS: {
-                    CliGenericResponse cliGenericResponse = agent.listComponents();
-                    responseMessage = getSuccessfulResponseMessage(cliGenericResponse, applicationMessage.getVersion());
-                    break;
-                }
-                case RESTART_COMPONENT: {
-                    RestartComponentRequest request = CBOR_MAPPER
-                            .readValue(applicationMessage.getPayload(), RestartComponentRequest.class);
-                    try {
-                        CliGenericResponse cliGenericResponse = agent.restartComponent(request);
-                        responseMessage = getSuccessfulResponseMessage(cliGenericResponse, applicationMessage
-                                .getVersion());
-                    } catch (InvalidArgumentsError | ComponentNotFoundError e) {
-                        responseMessage = getErrorResponseMessage(e, applicationMessage.getVersion());
-                    }
-                    break;
-                }
-                case STOP_COMPONENT: {
-                    StopComponentRequest request = CBOR_MAPPER
-                            .readValue(applicationMessage.getPayload(), StopComponentRequest.class);
-                    try {
-                        CliGenericResponse cliGenericResponse = agent.stopComponent(request);
-                        responseMessage = getSuccessfulResponseMessage(cliGenericResponse, applicationMessage
-                                .getVersion());
-                    } catch (InvalidArgumentsError | ComponentNotFoundError e) {
-                        responseMessage = getErrorResponseMessage(e, applicationMessage.getVersion());
-                    }
-                    break;
-                }
-                case UPDATE_RECIPES_AND_ARTIFACTS: {
-                    UpdateRecipesAndArtifactsRequest request = CBOR_MAPPER
-                            .readValue(applicationMessage.getPayload(), UpdateRecipesAndArtifactsRequest.class);
-                    try {
-                        agent.updateRecipesAndArtifacts(request);
-                        CliGenericResponse cliGenericResponse = new CliGenericResponse();
-                        cliGenericResponse.setMessageType(CliGenericResponse.MessageType.APPLICATION_MESSAGE);
-                        responseMessage = getSuccessfulResponseMessage(cliGenericResponse,
-                                applicationMessage.getVersion());
-                    } catch (InvalidArgumentsError | InvalidRecipesDirectoryPathError
-                            | InvalidArtifactsDirectoryPathError e) {
-                        responseMessage = getErrorResponseMessage(e, applicationMessage.getVersion());
-                    }
-                    break;
-                }
-                case CREATE_LOCAL_DEPLOYMENT: {
-                    CreateLocalDeploymentRequest request = CBOR_MAPPER
-                            .readValue(applicationMessage.getPayload(), CreateLocalDeploymentRequest.class);
-                    CliGenericResponse cliGenericResponse = agent.createLocalDeployment(config, request);
-                    responseMessage = getSuccessfulResponseMessage(cliGenericResponse, applicationMessage.getVersion());
-                    break;
-                }
-                case GET_LOCAL_DEPLOYMENT_STATUS: {
-                    GetLocalDeploymentStatusRequest request = CBOR_MAPPER
-                            .readValue(applicationMessage.getPayload(), GetLocalDeploymentStatusRequest.class);
-                    try {
-                        CliGenericResponse cliGenericResponse = agent.getLocalDeploymentStatus(config, request);
-                        responseMessage = getSuccessfulResponseMessage(cliGenericResponse,
-                                applicationMessage.getVersion());
-                    } catch (InvalidArgumentsError | ResourceNotFoundError e) {
-                        responseMessage = getErrorResponseMessage(e, applicationMessage.getVersion());
-                    }
-                    break;
-                }
-                case LIST_LOCAL_DEPLOYMENTS: {
-                    CliGenericResponse cliGenericResponse = agent.listLocalDeployments(config);
-                    responseMessage = getSuccessfulResponseMessage(cliGenericResponse, applicationMessage.getVersion());
-                    break;
-                }
-                default:
-                    CliGenericResponse cliGenericResponse = new CliGenericResponse();
-                    cliGenericResponse.setMessageType(CliGenericResponse.MessageType.APPLICATION_ERROR);
-                    cliGenericResponse.setErrorType("Unknown request type " + opCode.toString());
-                    responseMessage = ApplicationMessage.builder().version(applicationMessage.getVersion())
-                            .payload(CBOR_MAPPER.writeValueAsBytes(cliGenericResponse)).build();
-                    break;
-            }
-
-            fut.complete(new FrameReader.Message(responseMessage.toByteArray()));
-        } catch (Throwable e) {
-            logger.atError().setEventType("cli-ipc-error").setCause(e).log("Failed to handle message");
-            try {
-                ServiceError error = new ServiceError(e.getMessage());
-                ApplicationMessage responseMessage = getErrorResponseMessage(error, applicationMessage.getVersion());
-                fut.complete(new FrameReader.Message(responseMessage.toByteArray()));
-            } catch (IOException ex) {
-                logger.atError("cli-ipc-error", ex).log("Failed to send error response");
-            }
-        }
-        if (!fut.isDone()) {
-            fut.completeExceptionally(new IPCException("Unable to serialize any responses"));
-        }
-        return fut;
-    }
-
-    private ApplicationMessage getSuccessfulResponseMessage(CliGenericResponse cliGenericResponse, int version)
-            throws JsonProcessingException {
-        cliGenericResponse.setMessageType(CliGenericResponse.MessageType.APPLICATION_MESSAGE);
-        return ApplicationMessage.builder().version(version).payload(CBOR_MAPPER.writeValueAsBytes(cliGenericResponse))
-                .build();
-    }
-
-    private ApplicationMessage getErrorResponseMessage(GenericCliIpcServerException e, int version)
-            throws JsonProcessingException {
-        e.setMessageType(CliGenericResponse.MessageType.APPLICATION_ERROR);
-        e.setErrorType(e.getClass().getSimpleName());
-        return ApplicationMessage.builder().version(version).payload(CBOR_MAPPER.writeValueAsBytes(e)).build();
     }
 }

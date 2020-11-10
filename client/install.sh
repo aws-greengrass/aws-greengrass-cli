6010ac88d6b52bfdd82b615bb329dcd57ed2a5f3
Hui Yang <huiyn@amazon.com>
2020-11-09 17:25:47 -0800

    Fix group-based CLI auth; update version and install script

--- client/install.sh
+++ client/install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0" 2>/dev/null || echo "$(pwd)/""$0")")"
-ln -fs $SCRIPT_DIR/bin/greengrass-cli /usr/local/bin/greengrass-cli &2>/dev/null || sudo ln -fs $SCRIPT_DIR/bin/greengrass-cli /usr/local/bin/greengrass-cli
-echo "Start using greengrass-cli"
+ln -fs $SCRIPT_DIR/bin/greengrass-cli /usr/local/bin/greengrass-cli 2>/dev/null && echo "Start using greengrass-cli" || echo "Start using $SCRIPT_DIR/bin/greengrass-cli"

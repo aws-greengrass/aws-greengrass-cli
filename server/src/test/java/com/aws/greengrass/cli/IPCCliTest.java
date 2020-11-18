6fad6980c6990dfdfb979a4df07231980450db51
James Gosling <jag@amazon.com>
2020-11-18 12:31:27 -0800

    rebase

--- server/src/test/java/com/aws/greengrass/cli/IPCCliTest.java
+++ server/src/test/java/com/aws/greengrass/cli/IPCCliTest.java
@@ -63,7 +63,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;

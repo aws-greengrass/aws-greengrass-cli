2ae3b4ae0149a51bd26d582ad54c000b2fe7ffd6
Francis Fu <fufranci@amazon.com>
2021-05-10 16:26:16 -0700

    fix(windows): wait a little bit for the executorService to finish

--- client/src/main/java/com/aws/greengrass/cli/util/logs/impl/AggregationImpl.java
+++ client/src/main/java/com/aws/greengrass/cli/util/logs/impl/AggregationImpl.java
@@ -28,6 +28,7 @@ import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -151,7 +152,11 @@ public class AggregationImpl implements Aggregation {
      */
     @Override
     public void close() {
-        executorService.shutdownNow();
+        try {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /*

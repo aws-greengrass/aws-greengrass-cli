9d2fcc577e4cf5bf11c5b814ff02cde3b2a0dad2
Michael Dombrowski <mdombro@amazon.com>
2021-09-08 20:20:03 -0700

    fix(windows): handle symlinks when finding paths in cmd

--- client/src/bin/greengrass-cli.cmd
+++ client/src/bin/greengrass-cli.cmd
@@ -1,6 +1,14 @@
 @echo off
+@REM set local scope for variables so we don't leak into the caller's environment
+SETLOCAL
 
 set SCRIPT_PATH=%~dp0
+
+@REM Trace symlink to real path if we were launched from a symlink
+for /f "tokens=2 delims=[]" %%H in  ('dir /al %SCRIPT_PATH* ^| findstr /i /c:"%~nx0"') do (
+    set SCRIPT_PATH=%%~dpH
+)
+
 set CLI_HOME=%SCRIPT_PATH%..
 If Not Defined GGC_ROOT_PATH (
     set GGC_ROOT_PATH=%CLI_HOME%\..\..\..\..\..\..
